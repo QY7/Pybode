f890812544f16ae81dbeb7025050c409bc5520d7
YuQi <qiyu_sjtu@163.com>
2023-05-17 13:38:50 +0800

    完善零极点加入

--- window.py
+++ window.py
@@ -4,6 +4,9 @@ from plot_figure import Figure_Canvas
 import numpy as np
 import control as ct
 import matplotlib.pyplot as plt
+
+from PyQt5.QtCore import *
+
 class MainWindow(QMainWindow,Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
@@ -14,13 +17,18 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         self.fig_step_resp  = Figure_Canvas()
         self.fig_nyquist    = Figure_Canvas()
         s = ct.tf('s')
-        self.ct_sys = (s+1)/(s**2+1)
+        self.ct_gain = ct.tf([1],[1])
+        self.ct_sys = self.ct_gain
         self.freq_min = 0.01
         self.freq_max = 100000
         self.bode_mag_plot.addWidget(self.fig_bode_mag)
         self.bode_phase_plot.addWidget(self.fig_bode_phase)
         self.step_resp_plot.addWidget(self.fig_step_resp)
         self.nyquist_plot.addWidget(self.fig_nyquist)
+        self.shift_state = False
+        self.ctrl_state = False
+        self.pole_arr = [100]
+        self.zero_arr = []
         self.draw_figure()
 
     @property
@@ -64,10 +72,29 @@ class MainWindow(QMainWindow,Ui_MainWindow):
             plot=False,
             omega = np.geomspace(self.freq_min,self.freq_max,100000)
         )
-        _,nyquist_contour = ct.nyquist(ct_sys,plot=False,return_contour=True)
-        nyq_resp = ct_sys(nyquist_contour)
-        nyq_x,nyq_y = nyq_resp.real.copy(),nyq_resp.imag.copy()
-
+        try:
+            _,nyquist_contour = ct.nyquist(ct_sys,plot=False,return_contour=True)
+            nyq_resp = ct_sys(nyquist_contour)
+            nyq_x,nyq_y = nyq_resp.real.copy(),nyq_resp.imag.copy()
+            self.fig_nyquist.add_line(
+                nyq_x,
+                nyq_y,
+                x_min = -10,
+                x_max = 10,
+                y_min=-10,
+                y_max = 10
+            )
+            self.fig_nyquist.add_line(
+                nyq_x,
+                -nyq_y,
+                x_min = -10,
+                x_max = 10,
+                y_min=-10,
+                y_max = 10,
+                style='--'
+            )
+        except:
+            pass
         step_time,step_resp = ct.step_response(ct_sys)
         self.fig_bode_mag.add_line(
             omega,20*np.log(mag),
@@ -86,23 +113,7 @@ class MainWindow(QMainWindow,Ui_MainWindow):
             y_min =-200,
             y_max = 200
         )
-        self.fig_nyquist.add_line(
-            nyq_x,
-            nyq_y,
-            x_min = -10,
-            x_max = 10,
-            y_min=-10,
-            y_max = 10
-        )
-        self.fig_nyquist.add_line(
-            nyq_x,
-            -nyq_y,
-            x_min = -10,
-            x_max = 10,
-            y_min=-10,
-            y_max = 10,
-            style='--'
-        )
+
         self.fig_step_resp.add_line(
             step_time,
             step_resp,
@@ -112,7 +123,17 @@ class MainWindow(QMainWindow,Ui_MainWindow):
             y_max = 10
         )
         cid = self.fig_bode_mag.mpl_connect('button_press_event', self.add_pole_zero)
+        self.refresh_figure()
+
     def refresh_figure(self):
+        self.ct_sys = self.ct_gain
+        for item in self.pole_arr:
+            tmp_blk = ct.tf([1],[1/item,1])
+            self.ct_sys = ct.series(self.ct_sys,tmp_blk)
+        for item in self.zero_arr:
+            tmp_blk = ct.tf([1/item,1],[1])
+            self.ct_sys = ct.series(self.ct_sys,tmp_blk)
+
         mag,phase,omega = ct.bode(
             self.ct_sys,
             plot=False,
@@ -135,6 +156,25 @@ class MainWindow(QMainWindow,Ui_MainWindow):
             x_data = omega,
             y_data = phase
         )
+    # 检测键盘回车按键，函数名字不要改，这是重写键盘事件
+    def keyPressEvent(self, event):
+        print(event.key())
+        if(event.key() == 16777248):
+            self.shift_state = True
+        elif(event.key() == 16777249):
+            self.ctrl_state = True
+
+    def keyReleaseEvent(self,event):
+        if(event.key() == 16777248):
+            self.shift_state = False
+        elif(event.key() == 16777249):
+            self.ctrl_state = False
 
     def add_pole_zero(self,event):
-        print(event.xdata)
+        if self.shift_state:
+            print(f"Adding pole at {event.xdata}")
+            self.pole_arr.append(event.xdata)
+        elif(self.ctrl_state):
+            print(f"Adding zero at {event.xdata}")
+            self.zero_arr.append(event.xdata)
+        self.refresh_figure()
